867446e6958f2fcc44f4b0d600ef7cd66d553c3e
nombre <malenasando@gmail.com>
2024-07-05 03:58:20 -0300

    creacion de funcionalidad de inicio de sesion y funcion limpiarInputs para vaciar el formulario de registro

--- src/app/modules/autentificacion/pages/registro/registro.component.ts
+++ src/app/modules/autentificacion/pages/registro/registro.component.ts
@@ -4,46 +4,64 @@ import { Component } from '@angular/core';
 // IMPORTACION DE INTERFAZ UUSUARIO
 import { Usuario } from 'src/app/models/usuario';
 
-
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent {
-    // Este "hide" es para el input de contraseña
-    hide = true;
+  
+  // Este "hide" es para el input de contraseña
+  hide = true;
 
-    // IMPORTACIÓN DEL MODELO / INTERFAZ
-    usuarios: Usuario = {
-      uid: '',
-      nombre: '',
-      apellido: '',
-      email: '',
-      rol: '',
-      password: ''
+  // IMPORTACIÓN DEL MODELO / INTERFAZ
+  usuarios: Usuario = {
+    uid: '',
+    nombre: '',
+    apellido: '',
+    email: '',
+    rol: '',
+    password: ''
+  }
+  // CREAR UNA COLECCIÓN QUE SOLO RECIBE OBJETOS DEL TIPO USUARIOS
+  coleccionUsuarios: Usuario[] = [];
+
+  // FUNCIÓN PARA EL REGISTRO
+  registrar(){
+
+    // CREDENCIALES = información que ingrese el usuario
+    const credenciales = {
+      uid: this.usuarios.uid,
+      nombre: this.usuarios.nombre,
+      apellido: this.usuarios.apellido,
+      email: this.usuarios.email,
+      rol: this.usuarios.rol,
+      password: this.usuarios.password
     }
-  
-    // CREAR UNA COLECCIÓN QUE SOLO RECIBE OBJETOS DEL TIPO USUARIOS
-    coleccionUsuarios: Usuario[] = [];
-  
-    // FUNCIÓN PARA REGISTRO
-    registrar(){
-      // CREDENCIALES = información que ingrese el usuario
-      const credenciales = {
-        uid: this.usuarios.uid,
-        nombre: this.usuarios.nombre,
-        apellido: this.usuarios.apellido,
-        email: this.usuarios.email,
-        rol: this.usuarios.rol,
-        password: this.usuarios.password
-      }
-  
-      // enviamos los nuevos registros por medio del método push a la colección
-      this.coleccionUsuarios.push(credenciales);
-  
-      // por consola
-      console.log(credenciales);
-      console.log(this.coleccionUsuarios)
+
+    // enviamos los nuevos registros por medio del método push a la colección
+    this.coleccionUsuarios.push(credenciales);
+
+    // Notificamos al usuario el correcto registro
+    alert("Te registraste con éxito :)");
+
+    // Llamamos a la función limpiarInputs() para que se ejecute
+    this.limpiarInputs();
+
+    // por consola
+    // console.log(credenciales);
+    // console.log(this.coleccionUsuarios)
+  }
+
+  // Función para vaciar el formulario
+  limpiarInputs(){
+    const inputs = {
+      uid: this.usuarios.uid = '',
+      nombre: this.usuarios.nombre = '',
+      apellido: this.usuarios.apellido = '',
+      email: this.usuarios.email = '',
+      rol: this.usuarios.rol = '',
+      password: this.usuarios.password = ''
     }
+  }
 }
